74c437ae945ea07abf6e95ccc75c7c846995382a
Henrik Nergaard <henrik.nergaard@eplehuset.no>
2024-09-10 14:23:44 +0200

    Update Checkout with storefront API 3

--- netlify/functions/update-cart.js
+++ netlify/functions/update-cart.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context) {
     const shopifyStorefrontToken = process.env.Storefront_test_token; // Your Storefront API token
     const shopifyStore = 'eplehusettest'; // Replace with your actual Shopify store URL
-    const graphqlEndpoint = `https://${shopifyStore}.myshopify.com/admin/api/2024-07/carts.json`;
+    const graphqlEndpoint = `https://${shopifyStore}/api/2024-07/graphql.json`;
 
     const { id, quantity } = JSON.parse(event.body);
 
